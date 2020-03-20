87f0cad7ba484d5b04d1a3d4e58ed9612663031e
Dawid <david.d.bialy@gmail.com>
2020-03-20 23:52:50 +0000

    - Button text is now stored in a variable

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import ReactDOM from 'react-dom';
 // Create a React component
 
 const App = () => {
+    const buttonText = 'Click Me!';
+
     return (
       <div>
         <label className="label" for="name">Enter name:</label>
-        <input id="name" type="text" />
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
       </div>
     );
 };
